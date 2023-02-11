20a1dcd6d1996754aef90b82476b923366ccf1ea
Maximiliano Gutierrez <maxigutierrez23@gmail.com>
2023-02-11 20:08:13 -0300

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,23 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_KEY}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_KEY}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
         <title>Awesome Catan Board Generator</title>
         <meta
           name="description"
